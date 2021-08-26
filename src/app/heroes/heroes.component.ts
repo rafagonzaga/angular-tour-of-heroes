c31b8c254bfb7dbcc2b36a27256683188df18cdb
Rafael Gonzaga <ralozaga@bol.com.br>
2021-08-26 15:43:13 -0300

    Add service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +11,8 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   
   // hero: Hero = {
@@ -17,12 +20,19 @@ export class HeroesComponent implements OnInit {
     //   name: 'Windstorm'
     // };
     
-    constructor() { }
+    constructor(private heroService: HeroService, private messageService: MessageService) { }
     
     ngOnInit(): void {
+      this.getHeroes();
     }
     
     onSelect(hero: Hero): void {
       this.selectedHero = hero;
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+    }
+
+    getHeroes(): void {
+      // this.heroes = this.heroService.getHeroes();
+      this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
     }
 }
